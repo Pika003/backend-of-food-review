2332bb7ae40f8d084c1833eee8ba5997da5ac426
Pika003 <satyajitmaity0123.com>
2024-09-25 20:12:54 +0530

    multer edit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,17 @@
 import multer from "multer";
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      const uploadPath = path.join(__dirname, 'public');  // Absolute path
       cb(null, "./public")
     },
     filename: function (req, file, cb) {
       
       cb(null, file.originalname)
     }
-  })
+  });
   
 export const upload = multer({ 
     storage, 
-})
+});
